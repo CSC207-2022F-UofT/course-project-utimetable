d374d746ea1f711227cad887b3a2070a05d139ef
oshley213 <oshley0103@gmail.com>
2022-11-16 14:08:39 -0500

    FriendsList Class Completed.

--- src/main/java/entities/FriendsList.java
+++ src/main/java/entities/FriendsList.java
@@ -1,5 +1,24 @@
 package entities;
 
-public class FriendsList {
+import java.util.ArrayList;
 
+public class FriendsList {
+    ArrayList friends;
+    ArrayList blocked;
+
+    public ArrayList getFriends() {
+        return friends;
+    }
+
+    public ArrayList getBlocked() {
+        return blocked;
+    }
+
+    public void setFriends(ArrayList friendNames) {
+        friends = friendNames;
+    }
+
+    public void setBlocked(ArrayList blockedNames) {
+        blocked = blockedNames;
+    }
 }

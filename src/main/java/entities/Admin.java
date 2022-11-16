4951fb1bbaf8b9706c6f71528e119a83da84312d
naraelee <nar.lee@mail.utoronto.ca>
2022-11-16 16:35:41 -0500

    .....

--- src/main/java/entities/Admin.java
+++ src/main/java/entities/Admin.java
@@ -1,1 +1,25 @@
+package entities;
+import entities.User;
 
+public class Admin extends User {
+    String username;
+    String password;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

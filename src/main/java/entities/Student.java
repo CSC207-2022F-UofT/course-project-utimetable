da9b05d2c76637b12889166c55b22b0c56627fa0
naraelee <nar.lee@mail.utoronto.ca>
2022-11-16 16:23:18 -0500

    ...

--- src/main/java/entities/Student.java
+++ src/main/java/entities/Student.java
@@ -5,4 +5,22 @@ public class Student {
     String username;
     String password;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
+
+

7e187efc4093e22e2141c7fc783f881d078ae68c
Soomi Choi <schoi7439@gmail.com>
2022-11-16 16:55:59 -0500

    ...

--- src/main/java/entities/Admin.java
+++ src/main/java/entities/Admin.java
@@ -1,1 +1,26 @@
+package entities;
+import entities.User;
+
+public class Admin extends User {
+    String username;
+    String password;
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public void setUsername() {
+        this.username = username;
+    }
+    @Override
+    public void setPassword() {
+        this.password = password;
+    }
+}
 
--- src/main/java/entities/Student.java
+++ src/main/java/entities/Student.java
@@ -7,11 +7,26 @@ public class Student extends User {
 
     @Override
     public String getUsername() {
-        return username;
+        return this.username;
     }
     @Override
     public String getPassword() {
-        return password;
+        return this.password;
+    }
+    public Timetable getTimetable() {
+        return this.timetable;
+    }
+    public Timetable getBlankTimetable() {
+        Timetable blankTimetable = new String[5][12];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 12; j++) {
+                blankTimetable[i][j] = "#";
+            }
+        }
+        return blankTimetable;
+    }
+    public Location getLocation() {
+        return this.location;
     }
 
     @Override
@@ -21,5 +36,14 @@ public class Student extends User {
     @Override
     public void setPassword(String password) {
         this.password = password;
+    }
+    public void setTimetable() {
+        this.timetable = this.getBlankTimetable();
+    }
+    public void setLocation() {
+        if (this.timetable == this.getBlankTimetable()) {
+            return null;
+        }
+        this.location = this.getTimetable().getCourseInfo();
     }
 }

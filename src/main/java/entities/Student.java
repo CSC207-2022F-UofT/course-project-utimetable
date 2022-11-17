b7fd0b0cab31eabeeb364313a73040185394c9d3
Clarence <clarence.comsci@gmail.com>
2022-11-17 02:48:19 -0500

    added skeleton for functions

--- src/main/java/entities/Student.java
+++ src/main/java/entities/Student.java
@@ -22,4 +22,29 @@ public class Student extends User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public Timetable getTimetable() {
+        // TODO
+        // A getter function that returns the user's imported timetable
+    }
+
+    public Timetable getBlankTimetable() {
+        // TODO
+        // A getter function that returns an empty timetable with 5 rows and 12 columns
+    }
+
+    public void setTimetable(Timetable timetable) {
+        // TODO
+        // A setter function that sets an empty timetable
+    }
+
+    public Location getLocation() {
+        // TODO
+        // A getter function that returns the user's current location
+    }
+
+    public void setLocation() {
+        // TODO
+        // A setter function that sets the location of the user
+    }
 }

5b550d56fb9c2f82586df6453357a54aaed2655e
Jeff Huang <discussion.abstract@gmail.com>
2022-11-18 18:34:08 -0500

    Timetable init

---
+++ src/main/java/entities/Interval.java
@@ -0,0 +1,4 @@
+package entities;
+
+public record Interval(String courseCode, int start, int end) {
+}
--- src/main/java/entities/Timetable.java
+++ src/main/java/entities/Timetable.java
@@ -1,1 +1,22 @@
 
+package entities;
+
+import java.util.ArrayList;
+
+public class Timetable {
+
+    ArrayList<Interval> intervals;
+
+    public Timetable(ArrayList<Interval> intervals) {
+        this.intervals = intervals;
+    }
+
+
+    public ArrayList<Interval> getIntervals() { return this.intervals; }
+
+    public void addInterval(Interval interval) {
+        this.intervals.add(interval);
+    }
+
+}
+

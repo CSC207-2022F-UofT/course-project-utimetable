87b450be15b1a4adc32ba9334ca798e63bdf1fbe
oshley213 <oshley0103@gmail.com>
2022-11-19 18:46:50 -0500

    Modifications on FriendsListManager

--- src/main/java/useCases/FriendsListManager.java
+++ src/main/java/useCases/FriendsListManager.java
@@ -7,39 +7,67 @@ import java.util.ArrayList;
 
 public class FriendsListManager {
     public void addFriends(Student student1, Student student2) {
-        ArrayList<Student> friends = student1.getFriends();
-        friends.add(student2);
-        student1.setFriends(friends);
+        if (student1.getFriends().contains(student2)) {
+            ;
+        } else {
+            ArrayList<Student> friends = student1.getFriends();
+            friends.add(student2);
+            student1.setFriends(friends);
+        }
     }
 
     public void removeFriends(Student student1, Student student2) {
-        ArrayList<Student> friends = student1.getFriends();
-        friends.remove(student2);
-        student1.setFriends(friends);
+        if (student1.getFriends().contains(student2)) {
+            ArrayList<Student> friends = student1.getFriends();
+            friends.remove(student2);
+            student1.setFriends(friends);
+        } else {
+            ;
+        }
     }
 
     public void blockFriends(Student student1, Student student2) {
-        ArrayList<Student> blocked = student1.getBlocked();
-        blocked.add(student2);
-        student1.setBlocked(blocked);
+        if (student1.getFriends().contains(student2)) {
+            removeFriends(student1, student2);
+        }
+
+        if (student1.getBlocked().contains(student2)) {
+            ;
+        } else {
+            ArrayList<Student> blocked = student1.getBlocked();
+            blocked.add(student2);
+            student1.setBlocked(blocked);
+        }
     }
 
     public void unblockFriends(Student student1, Student student2) {
-        ArrayList<Student> blocked = student1.getBlocked();
-        blocked.remove(student2);
-        student1.setBlocked(blocked);
+        if (student1.getBlocked().contains(student2)) {
+            ArrayList<Student> blocked = student1.getBlocked();
+            blocked.remove(student2);
+            student1.setBlocked(blocked);
+        } else {
+            ;
+        }
     }
 
     public void starFriends(Student student1, Student student2) {
-        ArrayList<Student> starred = student1.getFriends();
-        starred.add(0, student2);
-        student1.setFriends(starred);
+        if (student1.getFriends().contains(student2)) {
+            ArrayList<Student> starred = student1.getFriends();
+            starred.add(0, student2);
+            student1.setFriends(starred);
+        } else {
+            ;
+        }
     }
 
     public void unstarFriends(Student student1, Student student2) {
-        ArrayList<Student> starred = student1.getFriends();
-        starred.remove(student2);
-        starred.add(student2);
-        student1.setFriends(starred);
+        if (student1.getFriends().contains(student2)) {
+            ArrayList<Student> starred = student1.getFriends();
+            starred.remove(student2);
+            starred.add(student2);
+            student1.setFriends(starred);
+        } else {
+            ;
+        }
     }
 }

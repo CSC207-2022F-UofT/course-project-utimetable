29b806cf43593111d3faaf647c7190e592479c2b
oshley213 <oshley0103@gmail.com>
2022-11-19 17:26:19 -0500

    Modifications on FriendsListManager

--- src/main/java/useCases/FriendsListManager.java
+++ src/main/java/useCases/FriendsListManager.java
@@ -6,41 +6,40 @@ import entities.Student;
 import java.util.ArrayList;
 
 public class FriendsListManager {
-    FriendsList friendsList = new FriendsList();
     public void addFriends(Student student1, Student student2) {
         ArrayList<Student> friends = student1.getFriends();
         friends.add(student2);
-        friendsList.setFriends(friends);
+        student1.setFriends(friends);
     }
 
     public void removeFriends(Student student1, Student student2) {
         ArrayList<Student> friends = student1.getFriends();
         friends.remove(student2);
-        friendsList.setFriends(friends);
+        student1.setFriends(friends);
     }
 
     public void blockFriends(Student student1, Student student2) {
         ArrayList<Student> blocked = student1.getBlocked();
         blocked.add(student2);
-        friendsList.setBlocked(blocked);
+        student1.setBlocked(blocked);
     }
 
     public void unblockFriends(Student student1, Student student2) {
         ArrayList<Student> blocked = student1.getBlocked();
         blocked.remove(student2);
-        friendsList.setBlocked(blocked);
+        student1.setBlocked(blocked);
     }
 
     public void starFriends(Student student1, Student student2) {
         ArrayList<Student> starred = student1.getFriends();
         starred.add(0, student2);
-        friendsList.setFriends(starred);
+        student1.setFriends(starred);
     }
 
     public void unstarFriends(Student student1, Student student2) {
         ArrayList<Student> starred = student1.getFriends();
         starred.remove(student2);
         starred.add(student2);
-        friendsList.setFriends(starred);
+        student1.setFriends(starred);
     }
 }

ff97693a2b65b46686ce93a9d016f92fd553c7b6
Soomi Choi <schoi7439@gmail.com>
2022-11-21 21:53:43 -0500

    db changes

--- src/main/java/database/UserModel.java
+++ src/main/java/database/UserModel.java
@@ -1,12 +1,47 @@
 package database;
 
+import entities.Student;
 import entities.Timetable;
+import entities.User;
 
 import java.util.ArrayList;
 
 public abstract class UserModel {
-    private String username;
-    private String password;
-    private ArrayList<String> friendList;
-    private Timetable timetable;
+
+    public String username;
+
+    public String password;
+
+    public ArrayList<Student> friends;
+    public ArrayList<Student> blocked;
+
+    public Timetable timetable;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Student> getFriendsList() {
+        return friends;
+    }
+
+    public ArrayList<Student> getBlockedList() {
+        return blocked;
+    }
+    public Timetable getTimetable() {
+        return timetable;
+    }
+
 }

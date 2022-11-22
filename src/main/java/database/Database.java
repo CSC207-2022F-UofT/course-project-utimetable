03f0386e0c6c38bd1aeaf7913d8a916e94181d3d
Clarence <clarence.comsci@gmail.com>
2022-11-22 07:22:59 -0500

    test

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -1,1 +1,24 @@
 
+package database;
+
+import database.UserModel;
+import entities.User;
+import entities.Student;
+import entities.Admin;
+
+import java.util.ArrayList;
+
+class Database {
+
+    private ArrayList<UserModel> database;
+    public ArrayList<UserModel> getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(ArrayList<UserModel> database) {
+        this.database = database;
+    }
+
+
+
+}

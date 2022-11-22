2db458f59e191030515aa3a1f753714d4baaec07
Jeff Huang <discussion.abstract@gmail.com>
2022-11-21 19:59:57 -0500

    database

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -3,3 +3,20 @@ package database;
 import entities.User;
 import entities.Student;
 import entities.Admin;
+
+import java.util.ArrayList;
+
+class Database {
+
+    private ArrayList<UserModel> database;
+    public ArrayList<UserModel> getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(ArrayList<UserModel> database) {
+        this.database = database;
+    }
+
+
+
+}

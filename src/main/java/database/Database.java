30bf969440d6d485b518ee3b6ec6a8dc9ad9e5da
Soomi Choi <schoi7439@gmail.com>
2022-11-21 21:02:53 -0500

    Modifications in Database, SignIn, SignUp

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -3,3 +3,25 @@ package database;
 import entities.User;
 import entities.Student;
 import entities.Admin;
+
+import java.util.ArrayList;
+
+class Database {
+
+    private ArrayList<UserModel> database;
+    public ArrayList<UserModel> getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(ArrayList<UserModel> database) {
+        this.database = database;
+    }
+
+    public void addNewUser(String username, String password) {
+        UserModel userModel = null;
+        userModel.setUsername(username);
+        userModel.setPassword(password);
+        this.database.add(userModel);
+    }
+
+}
